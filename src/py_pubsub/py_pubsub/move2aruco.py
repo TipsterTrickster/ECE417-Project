45431f439fb62bd9d42558ee3a86d82f9336ee11
Aedan Bryant <aedan.bryant@maine.edu>
2025-12-10 17:12:26 -0500

    Chains aruco markers together to get home relative position

--- src/py_pubsub/py_pubsub/move2aruco.py
+++ src/py_pubsub/py_pubsub/move2aruco.py
@@ -23,9 +23,9 @@ OMINANGLE = 5 * math.pi/180.
 # if angular velocity is smaller than this, then motors are unable to move the jetbot
 OMIN = 0.50
 
-def get_2d_rot_matrix(quat):
+def get_3d_rot_matrix(quat):
     """
-    Get 2d rotation matrix (ignore y axis, tilt) from quaternion 
+    Get 3d rotation matrix (ignore y axis, tilt) from quaternion 
     """
 
     # Initialiation of quarternion variables
@@ -39,7 +39,26 @@ def get_2d_rot_matrix(quat):
     ])
 
     # Get 2x2 matrix without Y axis
-    return np.array([[rotation_matrix[0,0], rotation_matrix[0, 2]], [rotation_matrix[2,0], rotation_matrix[2, 2]]])
+    # return np.array([[rotation_matrix[0,0], rotation_matrix[0, 2]], [rotation_matrix[2,0], rotation_matrix[2, 2]]])
+    return rotation_matrix
+
+# def get_inverted_transformation_matrix(R_matrix, t_matrix):
+
+#     RT_matrix = np.transpose(R_matrix)
+
+#     transformation = np.vstack([
+#         np.hstack([RT_matrix, (-1 * RT_matrix) @ t_matrix.reshape(2, 1)]),
+#         np.array([0, 0, 1])
+#     ])
+
+#     return transformation
+def get_inverted_transformation_matrix(T, D=3):
+    Tinv = np.eye(D+1)
+    Tinv[:D, :D] = T[:D, :D].T
+    Tinv[:D, D] = - T[:D, :D].T @ T[:D, D]
+    return Tinv
+
+
 
 class GetPos(Node):
     def __init__(self):
@@ -51,10 +70,13 @@ class GetPos(Node):
         self.on_aruco_detection, 10)
 
         # Will store the origin reference marker
-        self.home_marker = None
+        self.home_marker = 0
 
         # Will store transformation matrices from each tranformation matrix to eachother transformation matrix
         self.aruco_transformation_matrices = {}
+
+        # Stores the transformation matrices from each marker to the home marker
+        self.aruco_home_transformation_matrices = {}
 
         # No Timer
         self.timer = None
@@ -68,38 +90,90 @@ class GetPos(Node):
 
         # Dictionary of transformations from robot to all markers in view
         transformations = {}
+        inverse_transformations = {}
 
         for marker in msg.markers:
             # Displays message along with a time stamp
-            self.get_logger().info('marker: "%s"' % marker.marker_id)
+            # self.get_logger().info('marker: "%s"' % marker.marker_id)
 
             # Get rotation and translation matrices
-            rotation = get_2d_rot_matrix((marker.pose.orientation.w, marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z))
+            rotation = get_3d_rot_matrix((marker.pose.orientation.w, marker.pose.orientation.x, marker.pose.orientation.y, marker.pose.orientation.z))
 
-            translation = np.array([marker.pose.position.x, marker.pose.position.z])
+            # translation = np.array([marker.pose.position.x, marker.pose.position.z])
+            translation = np.array([marker.pose.position.x, marker.pose.position.y, marker.pose.position.z])
 
             # Create Robot to Marker Transformation matrix
             # R T M
             transformation = np.vstack([
-                np.hstack([rotation, translation.reshape(2, 1)]),
-                np.array([0, 0, 1])
+                np.hstack([rotation, translation.reshape(3, 1)]),
+                np.array([0, 0, 0, 1])
             ])
 
-            # Set a home marker if one hasn't been set yet
+            # Set a home marker to 0 once the marker comes into frame
             if self.home_marker == None:
-                self.home_marker = marker.marker_id
+                if marker.id == 0:
+                    self.home_marker = 0
             
             transformations[marker.marker_id] = transformation
-            
-            # Print debug information
-            self.get_logger().info('Rotation: "%s"' % rotation)
-            self.get_logger().info('Translation: "%s"' % translation)
-            self.get_logger().info('Transformation: "%s"' % transformation)
-            # self.get_logger().info('marker: "%s"' % marker.marker_id)
-            # self.get_logger().info('Z pose: "%s"' % marker.pose.position.z)
-            # self.get_logger().info('X pose: "%s"' % marker.pose.position.x)
 
-        self.get_logger().info('DONE')
+            # inverse_transformation = get_inverted_transformation_matrix(rotation, translation)
+            inverse_transformation = get_inverted_transformation_matrix(transformation)
+            inverse_transformations[marker.marker_id] = inverse_transformation
+
+
+        # Loop through normal and inverse transformations and get the transformation matrices between every pair of markers
+        for t_id in transformations.keys():
+            for ti_id in inverse_transformations.keys():
+                if ti_id == t_id:
+                    continue
+
+                relative_transformation = inverse_transformations[ti_id] @ transformations[t_id]
+                self.aruco_transformation_matrices[f"{str(ti_id)}{str(t_id)}"] = relative_transformation
+
+
+        # Add all home transformations directly
+        if self.home_marker is not None:
+            for id in self.aruco_transformation_matrices.keys():
+                if id[1] == '0':
+                    self.aruco_home_transformation_matrices[int(id[0])] = self.aruco_transformation_matrices[id]
+
+        
+        # Chain aruco marker transformations to get their positions relative to the home marker
+        for t_id in transformations.keys():
+
+            if t_id == 0:
+                continue
+            
+            # If the transformation doesn't already have a home transformation chain together some to get one
+            if t_id not in self.aruco_home_transformation_matrices.keys():
+
+                for a_id in self.aruco_transformation_matrices.keys():
+
+                    # If the transformation 
+                    if t_id == int(a_id[0]):
+                        if int(a_id[1]) in self.aruco_home_transformation_matrices.keys():
+
+                            self.aruco_home_transformation_matrices[t_id] = self.aruco_transformation_matrices[a_id] @ self.aruco_home_transformation_matrices[t_id]
+
+        # print("HOME TRANFORMATION MATRICES")
+
+        # for key in self.aruco_home_transformation_matrices.keys():
+        #     print(f"ArucoID: {key}")
+        #     print(self.aruco_home_transformation_matrices[key])
+
+        print("HOME RELATIVE LOCATION")
+
+        # Get the robots position relative to home
+        for t_id in transformations.keys():
+            if t_id == 0:
+                print(transformations[t_id])
+                break
+
+            print(transformations[t_id] @ self.aruco_home_transformation_matrices[t_id])
+            break
+                
+
+
 
 def main(args=None):
     # Initializes library
